655eb0394d6e43acd7ce1b1de6ebe61cc0af29e8
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-06-26 16:06:03 +0500

    git init .

---
+++ js/script.js
@@ -0,0 +1,112 @@
+"use strick";
+
+let btnAll = $(".all-btn");
+let addClick = $(".auto-click");
+let addCard = $(".js-add");
+let addBaby = $(".click-baby");
+let addMovie = $(".js-movie");
+let footCard = $(".footer__card");
+let footCard2 = $(".footer__card-2");
+let footCard3 = $(".footer__card-3");
+let footCard4 = $(".footer__card-4");
+let footCard5 = $(".footer__card-5");
+let footCard6 = $(".footer__card-6");
+
+let footCard7 = $(".footer__card-7");
+let footCard8 = $(".footer__card-8");
+let footCard9 = $(".footer__card-9");
+let footCard10 = $(".footer__card-10");
+let footCard11 = $(".footer__card-11");
+let footCard12 = $(".footer__card-12");
+let footCard13 = $(".footer__card-13");
+let footCard14 = $(".footer__card-14");
+let footCard15 = $(".footer__card-15");
+let footCard16 = $(".footer__card-16");
+
+addClick.addEventListener("click", () => {
+  footCard.style.display = "none";
+  footCard2.style.display = "none";
+  footCard3.style.display = "none";
+  footCard4.style.display = "none";
+
+  footCard5.style.display = "block";
+  footCard6.style.display = "block";
+  footCard7.style.display = "block";
+  footCard8.style.display = "block";
+
+  footCard13.style.display = "none";
+  footCard14.style.display = "none";
+  footCard15.style.display = "none";
+  footCard16.style.display = "none";
+
+
+
+  footCard9.style.display = "none";
+  footCard10.style.display = "none";
+  footCard11.style.display = "none";
+  footCard12.style.display = "none";
+});
+
+addBaby.addEventListener("click", () => {
+  footCard.style.display = "none";
+  footCard2.style.display = "none";
+  footCard3.style.display = "none";
+  footCard4.style.display = "none";
+  footCard5.style.display = "none";
+  footCard6.style.display = "none";
+  footCard7.style.display = "none";
+  footCard8.style.display = "none";
+
+  footCard9.style.display = "block";
+  footCard10.style.display = "block";
+  footCard11.style.display = "block";
+  footCard12.style.display = "block";
+
+  footCard13.style.display = "none";
+  footCard14.style.display = "none";
+  footCard15.style.display = "none";
+  footCard16.style.display = "none";
+});
+
+addMovie.addEventListener("click", () => {
+  footCard.style.display = "none";
+  footCard2.style.display = "none";
+  footCard3.style.display = "none";
+  footCard4.style.display = "none";
+  footCard5.style.display = "none";
+  footCard6.style.display = "none";
+  footCard7.style.display = "none";
+  footCard8.style.display = "none";
+
+  footCard9.style.display = "none";
+  footCard10.style.display = "none";
+  footCard11.style.display = "none";
+  footCard12.style.display = "none";
+
+  footCard13.style.display = "block";
+  footCard14.style.display = "block";
+  footCard15.style.display = "block";
+  footCard16.style.display = "block";
+});
+
+
+btnAll.addEventListener("click", ()=> {
+  footCard.style.display = "block";
+  footCard2.style.display = "block";
+  footCard3.style.display = "block";
+  footCard4.style.display = "block";
+  footCard5.style.display = "block";
+  footCard6.style.display = "block";
+  footCard7.style.display = "block";
+  footCard8.style.display = "block";
+
+  footCard9.style.display = "block";
+  footCard10.style.display = "block";
+  footCard11.style.display = "block";
+  footCard12.style.display = "block";
+
+  footCard13.style.display = "block";
+  footCard14.style.display = "block";
+  footCard15.style.display = "block";
+  footCard16.style.display = "block";
+})
